ef2099fe531e1763e4c81ca57f9e7ec853379a53
farhan aqaei <farhan9aqaei@gmail.com>
2025-03-23 04:58:15 -0700

    Add Command repository and interface for managing commands and platforms

---
+++ CommandService/Data/CommandRepo.cs
@@ -0,0 +1,53 @@
+using CommandService.Models;
+
+namespace CommandService.Data;
+public class CommandRepo(AppDbContext context) : ICommandRepo
+{
+    public void CreateCommand(int platformId, Command command)
+    {
+        if (command == null)
+        {
+            throw new ArgumentNullException(nameof(command));
+        }
+        command.PlatformId = platformId;
+        context.Commands.Add(command);
+    }
+
+    public void CreatePlatform(Platform platform)
+    {
+        if (platform == null)
+        {
+            throw new ArgumentNullException(nameof(platform));
+        }
+        context.Platforms.Add(platform);
+    }
+
+    public IEnumerable<Platform> GetAllPlatforms()
+    {
+        return context.Platforms.ToList();
+    }
+
+    public Command GetCommand(int platformId, int commandId)
+    {
+        return context.Commands
+            .Where(x => x.PlatformId == platformId && x.Id == commandId)
+            .FirstOrDefault();
+    }
+
+    public IEnumerable<Command> GetCommandsForPlatform(int platformId)
+    {
+        return context.Commands
+            .Where(x => x.PlatformId == platformId)
+            .OrderBy(x => x.Platform.Name);
+    }
+
+    public bool PlatformExists(int platformId)
+    {
+        return context.Platforms.Any(x => x.Id == platformId);
+    }
+
+    public bool SaveChanges()
+    {
+        return context.SaveChanges() >= 0;
+    }
+}
